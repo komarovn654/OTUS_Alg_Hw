a223491ed28f014f46aaa3a3db8f7fd0220b4ec8
Николай Комаров <n.komarov@atol.ru>
2023-04-02 17:49:59 +0300

    hw18

--- hw18_shortestpath/dijkstra/dijkstra.go
+++ hw18_shortestpath/dijkstra/dijkstra.go
@@ -26,26 +26,7 @@ func initPathways(len int) pathways {
 }
 
 func FindPath(g *graph.Graph, src int, dst int) []graph.Edge {
-	// path := initPathways(len(g.GetAdjacentVectors()))
 
-	// reachable := stack.New()
-	// for _, vertex := range g.GetAdjacentVectors()[0] {
-	// 	reachable.Push(vertex)
-	// }
-
-	// for vertex, ok := reachable.Pop(); ok; vertex, ok = reachable.Pop() {
-	// 	v, ok := vertex.(graph.AdjacentVertex)
-	// 	if !ok {
-	// 		panic("type cast error")
-	// 	}
-
-	// 	for _, vertex := range g.GetAdjacentVectors()[v.Dst] {
-	// 		reachable.Push(vertex)
-	// 	}
-	// 	fmt.Println(vertex)
-	// }
-
-	// return path[src].edges
 	bst.InitBst()
 
 	return nil
