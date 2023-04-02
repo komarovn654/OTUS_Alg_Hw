705c2a9fc1594558710f83c35c773952ae2b17d7
Николай Комаров <n.komarov@atol.ru>
2023-04-02 15:04:02 +0300

    hw18

--- hw18_shortestpath/dijkstra/dijkstra.go
+++ hw18_shortestpath/dijkstra/dijkstra.go
@@ -1,11 +1,10 @@
 package dijkstra
 
 import (
-	"fmt"
 	"math"
 
+	bst "github.com/komarovn654/OTUS_Alg_Hw/bst"
 	graph "github.com/komarovn654/OTUS_Alg_Hw/hw18_shortestpath"
-	stack "github.com/komarovn654/OTUS_Alg_Hw/hw18_shortestpath/stack"
 )
 
 type path struct {
@@ -27,26 +26,29 @@ func initPathways(len int) pathways {
 }
 
 func FindPath(g *graph.Graph, src int, dst int) []graph.Edge {
-	path := initPathways(len(g.GetAdjacentVectors()))
+	// path := initPathways(len(g.GetAdjacentVectors()))
 
-	reachable := stack.New()
-	for _, vertex := range g.GetAdjacentVectors()[0] {
-		reachable.Push(vertex)
-	}
+	// reachable := stack.New()
+	// for _, vertex := range g.GetAdjacentVectors()[0] {
+	// 	reachable.Push(vertex)
+	// }
 
-	for vertex, ok := reachable.Pop(); ok; vertex, ok = reachable.Pop() {
-		v, ok := vertex.(graph.AdjacentVertex)
-		if !ok {
-			panic("type cast error")
-		}
+	// for vertex, ok := reachable.Pop(); ok; vertex, ok = reachable.Pop() {
+	// 	v, ok := vertex.(graph.AdjacentVertex)
+	// 	if !ok {
+	// 		panic("type cast error")
+	// 	}
 
-		for _, vertex := range g.GetAdjacentVectors()[v.Dst] {
-			reachable.Push(vertex)
-		}
-		fmt.Println(vertex)
-	}
+	// 	for _, vertex := range g.GetAdjacentVectors()[v.Dst] {
+	// 		reachable.Push(vertex)
+	// 	}
+	// 	fmt.Println(vertex)
+	// }
 
-	return path[src].edges
+	// return path[src].edges
+	bst.InitBst()
+
+	return nil
 }
 
 func add(x int, y int) int {
--- hw18_shortestpath/go.mod
+++ hw18_shortestpath/go.mod
@@ -2,7 +2,10 @@ module github.com/komarovn654/OTUS_Alg_Hw/hw18_shortestpath
 
 go 1.18
 
-require github.com/stretchr/testify v1.8.2
+require (
+	github.com/komarovn654/OTUS_Alg_Hw/bst v0.0.0-20230402115627-d1f2ce4d96a9
+	github.com/stretchr/testify v1.8.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
